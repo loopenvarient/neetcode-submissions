cfd17065589fa269bb20c18bb90c0f5b4e8c48fc
Armeen Asim <armeen.aasim@gmail.com>
2026-09-01 22:52:57 +0500

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        #details ia a string which can have many passengers,
+        #length of each passemger is 15
+        #1-10 is phone, 11 is G, 12,13 combines age, 14,15 combines to give seat
+        count = 0
+        for x in details:
+            x = x.strip()
+
+            if int(x[11:13]) > 60:
+                count += 1
+        return count
